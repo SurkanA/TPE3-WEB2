04d547a0dd8ad8b4693fe38f3f2264e7ee31f5bd
Agustin Surkan <agustinsurkan@gmail.com>
2024-11-17 23:55:51 -0300

    Actualizacion de la API de jugador y paginacion

--- router.php
+++ router.php
@@ -1,13 +1,15 @@
 <?php
 require_once 'libs/router.php';
-
 require_once 'api/controllers/jugador.api.controller.php';
 
 $router = new Router();
 
+// Jugadores
 $router->addRoute('jugadores', 'GET', 'JugadorApiController', "obtenerJugadores");
-$router->addRoute('jugadores/:EQUIPO/:ID', 'GET', 'JugadorApiController', "obtenerJugador");
-$router->addRoute('jugadores/:EQUIPO/:ID', 'DELETE', 'JugadorApiController', "borrarJugador");
-$router->addRoute('jugadores', 'POST', 'JugadorApiController', "crearJugador");
+$router->addRoute('jugadores/:CANTIDAD', 'GET', 'JugadorApiController', "obtenerJugadoresPaginado");
+$router->addRoute('jugador/:EQUIPO/:ID', 'GET', 'JugadorApiController', "obtenerJugador");
+$router->addRoute('jugador/:EQUIPO/:ID', 'PUT', 'JugadorApiController', "actualizarJugador");
+$router->addRoute('jugador/:EQUIPO/:ID', 'DELETE', 'JugadorApiController', "borrarJugador");
+$router->addRoute('jugador', 'POST', 'JugadorApiController', "crearJugador");
 
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
